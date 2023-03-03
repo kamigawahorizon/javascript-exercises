c80048fb4725534619f169f469fa3ad64113517b
kamigawahorizon <nicolojacobtiu4@yahoo.com>
2023-03-03 15:25:53 +0800

    updated and answered eercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
+const reverseString = function (de_tali) {
+return de_tali.split("").reverse().join("");
+}
 
-};
+
+
+
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
